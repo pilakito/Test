5a65a13b2656f3fe5baa5bd1f4d9b681109c0935
pilakito <69727115+pilakito@users.noreply.github.com>
2023-03-14 15:23:35 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python3
-"""
-BLC Communication and Security Systems
-AYPOS Project PDU Data Recording
-
-author: @Burak Gumus
-"""
 
 import subprocess
 import datetime
